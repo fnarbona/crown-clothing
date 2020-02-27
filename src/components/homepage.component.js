3b12ca214e4c740ca0a99d6530bf6f59a9a8d7ae
Fernando Rafael Narbona <fernandorafaelnarbona@MacBook-Pro.local>
2020-02-27 17:54:17 -0500

    first commit

---
+++ src/components/homepage.component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div classname="homepage">
+    <div classname="directory-menu">
+      <div classname="menu-item">
+        <div classname="content">
+          <h1 classname="title">TITLE</h1>
+          <span classname="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+        <div classname="menu-item">
+          <div classname="content">
+            <h1 classname="title">TITLE</h1>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+          <div classname="menu-item">
+            <div classname="content">
+              <h1 classname="title">TITLE</h1>
+              <span classname="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
